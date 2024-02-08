32f9ba55ef8749f1f5c01166b799bb9e6f92bc20
akshithputtur <mydreams.pk@gmail.com>
2024-02-08 20:31:10 +0530

    modfied file for color

--- digitalclock.py
+++ digitalclock.py
@@ -5,8 +5,8 @@ app_window.title("Digital Clock")
 app_window.geometry("420x150") 
 app_window.resizable(1,1)
 
-text_font= ("Boulder", 68, 'bold')
-background = "#f2e750"
+text_font= ("Boulder", 100, 'bold')
+background = "#f2e755"
 foreground= "#363529"
 border_width = 25
 
